6b39f5e661758c0e5f43ece6abc77e9f6a625a17
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2023-05-25 16:15:38 -0300

    What-GPT3 compnent + responsive

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,7 +1,15 @@
-
-const Feature = () => {
+import './feature.css'
+const Feature = ({title, text}) => {
   return (
-    <div>Feature</div>
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt3__features-container_feature-text'>
+        <p>{text}</p>
+      </div>
+    </div>
   )
 }
 
